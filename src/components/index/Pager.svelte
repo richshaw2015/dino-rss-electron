b2f49d83bbc3a06eb734cb6d02f4c72b2d7df8a9
Neo <richshaw@126.com>
2020-09-22 10:37:02 +0800

    finish Pager components

---
+++ src/components/index/Pager.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let curPage = 2;
+    export let numPages = 9;
+    // export let newPage = 0;
+</script>
+
+<style>
+    #omr-pager {
+        width: 400px;
+        border-top: 1px dashed rgba(0,0,0,0.14);
+    }
+    .pagination {
+        display: flex;
+        justify-content: center;
+    }
+    .pagination li {
+        width: 14%;
+        margin: auto 3%;
+        border-radius: 4px;
+    }
+    .cur_page {
+        background: #ee6e73;
+    }
+    .cur_page a {
+        color: #fff;
+    }
+</style>
+
+<div id="omr-pager">
+    <ul class="pagination">
+        <li class="{ curPage > 1 ? 'waves-effect' : 'disabled' }" title="Previous Page">
+            <a href={'#'}><i class="material-icons">chevron_left</i></a></li>
+
+        <li class="{ curPage > 1 ? 'waves-effect' : 'disabled' }" title="First Page"><a href={'#'}>1</a></li>
+        <li class="cur_page" title="Current Page"><a href={'#'}>{curPage}</a></li>
+        <li class="{ curPage < numPages ? 'waves-effect' : 'disabled' }" title="Last Page">
+            <a href={'#'}>{ numPages }</a></li>
+
+        <li class="{ curPage < numPages ? 'waves-effect' : 'disabled' }" title="Next Page">
+            <a href={'#'}><i class="material-icons">chevron_right</i></a></li>
+    </ul>
+</div>
